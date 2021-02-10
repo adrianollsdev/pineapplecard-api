c359dd4232b0343de20c16aa4718e39ed167d0b4
Adriano Leão <adrianolls.dev@gmail.com>
2021-02-10 15:35:03 -0300

    bd inserts up

---
+++ bd/1_insert_establishment_categories.sql
@@ -0,0 +1,8 @@
+insert into establishment_categories(id,name,score_rate,created_at,updated_at) values('1','Outros','0.80','2021-02-05 04:06:32','2021-02-05 04:06:32');
+insert into establishment_categories(id,name,score_rate,created_at,updated_at) values('2','Casa','1.00','2021-02-05 04:06:32','2021-02-05 04:06:32');
+insert into establishment_categories(id,name,score_rate,created_at,updated_at) values('3','Saúde','1.40','2021-02-05 04:06:32','2021-02-05 04:06:32');
+insert into establishment_categories(id,name,score_rate,created_at,updated_at) values('4','Restaurantes','1.80','2021-02-05 04:06:32','2021-02-05 04:06:32');
+insert into establishment_categories(id,name,score_rate,created_at,updated_at) values('5','Serviços','2.30','2021-02-05 04:06:32','2021-02-05 04:06:32');
+insert into establishment_categories(id,name,score_rate,created_at,updated_at) values('6','Educação','0.30','2021-02-05 04:06:32','2021-02-05 04:06:32');
+insert into establishment_categories(id,name,score_rate,created_at,updated_at) values('7','Lojas','2.70','2021-02-05 04:06:32','2021-02-05 04:06:32');
+insert into establishment_categories(id,name,score_rate,created_at,updated_at) values('8','Comunicação','0.60','2021-02-05 04:06:33','2021-02-05 04:06:33');
---
+++ bd/3_insert_profiles.sql
@@ -0,0 +1,1 @@
+insert into profiles(id,name,date_of_birth,gender,total_limit,payment_day,created_at,updated_at) values('1','Adriano Leão','1997-11-11','m','6000.00','5','2021-02-05 04:11:02','2021-02-05 04:11:02');
